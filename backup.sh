f9287f3380b33f4dc8b3f73671fc56c4f2f605cf
Suyuimo <161992592+Suyuimo@users.noreply.github.com>
2024-05-03 00:06:00 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+#Variabeln
+smbserver=xxx
+smbusername=xxx
+smbpassword=xxx
+
+#Info an alle Spieler
+screen -S minecraft -X stuff "say Die Welt wird nun gesichert \n"
+screen -S minecraft -X stuff "say Der Server wird dabei kurz neugestartet
+
+./stop.sh
+
