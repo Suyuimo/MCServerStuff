8dc4e14c3c49901bf76afad90d049317c449e258
Suyuimo <161992592+Suyuimo@users.noreply.github.com>
2024-05-13 12:11:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,14 @@ noipusername=XXX
 noippassword=XXX
 noiphostname=all.ddnskey.com # all.ddnskey.com in meinem Bespiel.
 
+#noip v6 update?
+noipv6update=n # n für Nein, y für Ja
+
 # Server starten
 screen -d -m -S minecraft java -Xmx$javamax -jar $serverjarfile nogui
 screen -d -m -S noipv4 noip-duc -g all.ddnskey.com --username $noipusername --password $noippassword
+
+if [ $noipv6update == 'y' ]
+then
 screen -d -m -S noipv6 noip-duc -g all.ddnskey.com --username $noipusername --password $noippassword --ip-method http://ip1.dynupdate6.no-ip.com/
+fi
