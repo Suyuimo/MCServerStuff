ba5a294c751df65710db6fe9f5e7f32aacb22a68
Suyuimo <161992592+Suyuimo@users.noreply.github.com>
2024-09-08 15:46:58 +0200

    Create install-java.sh

---
+++ install-java.sh
@@ -0,0 +1,4 @@
+wget https://download.oracle.com/java/22/latest/jdk-22_linux-aarch64_bin.tar.gz
+tar xfvz jdk-22_linux-aarch64_bin.tar.gz
+mv jdk-22.0.2 /usr/lib/jvm/
+update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-22.0.2/bin/java 2082
